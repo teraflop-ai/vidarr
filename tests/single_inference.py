1340d9da1496a42599afa247007b624f16533505
Enrico Shippole <enricoship@gmail.com>
2025-12-09 21:25:31 -0500

    tests for upload and single image inference

---
+++ tests/single_inference.py
@@ -0,0 +1,26 @@
+from urllib.request import urlopen
+
+import timm
+import torch
+from PIL import Image
+
+img = Image.open(
+    urlopen(
+        "https://huggingface.co/datasets/huggingface/documentation-images/resolve/main/beignets-task-guide.png"
+    )
+)
+
+model = timm.create_model("hf_hub:TeraflopAI/compression-detection", pretrained=True)
+model = model.eval()
+
+# get model specific transforms (normalization, resize)
+data_config = timm.data.resolve_model_data_config(model)
+transforms = timm.data.create_transform(**data_config, is_training=False)
+
+tensor = transforms(img).unsqueeze(0)
+with torch.no_grad():
+    output = model(tensor)
+
+pred_scores = torch.argmax(output, dim=1).item()
+
+print(pred_scores)
---
+++ tests/upload.py
@@ -0,0 +1,44 @@
+import timm
+import torch
+
+from vidarr.utils import upload_to_hf
+
+model_name = "timm/efficientvit_b1.r288_in1k"
+num_classes = 2
+checkpoint_path = "/home/henry/vidarr/checkpoints/final_model.pt"
+device = "cuda"
+repo_id = "TeraflopAI/compression-detection"
+
+model = timm.create_model(model_name, num_classes=num_classes)
+state = torch.load(checkpoint_path, weights_only=True, map_location=device)
+model.load_state_dict(state)
+
+model_cfg = {
+    "architecture": "efficientvit_b1",
+    "num_classes": 2,
+    "num_features": 256,
+    "global_pool": "avg",
+    "pretrained_cfg": {
+        "tag": "r288_in1k",
+        "custom_load": False,
+        "input_size": [3, 288, 288],
+        "fixed_input_size": False,
+        "interpolation": "bicubic",
+        "crop_pct": 1.0,
+        "crop_mode": "center",
+        "mean": [0.485, 0.456, 0.406],
+        "std": [0.229, 0.224, 0.225],
+        "num_classes": 1000,
+        "pool_size": [9, 9],
+        "first_conv": "stem.in_conv.conv",
+        "classifier": "head.classifier.4",
+    },
+    "architectures": ["timm/efficientvit_b1.r288_in1k"],
+    "num_labels": 2,
+    "id2label": {1: "no_artifacts", 0: "jpeg_artifacts"},
+    "label2id": {"no_artifacts": 1, "jpeg_artifacts": 0},
+    "model_type": "image-classification",
+}
+
+
+upload_to_hf(model=model, model_cfg=model_cfg, repo_id=repo_id, private=False)
